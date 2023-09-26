638c773269cdf6bc804d791b04cf1688407f3a45
Trafficode <kkuras89@gmail.com>
2023-09-26 02:16:02 +0200

    remove flash operation

--- src/main.c
+++ src/main.c
@@ -91,49 +91,6 @@ int main(void) {
     LOG_INF("FIRMWARE_VERSION %s", FIRMWARE_VERSION);
     LOG_INF("ZEPHYR_VERSION %s", ZEPHYR_VERSION);
 
-    struct flash_pages_info info = {0};
-    Fs.flash_device = NVS_PARTITION_DEVICE;
-    if (!device_is_ready(Fs.flash_device)) {
-        LOG_ERR("Flash device %s is not ready", Fs.flash_device->name);
-        while (true)
-            ;
-    }
-    Fs.offset = NVS_PARTITION_OFFSET;
-    ret = flash_get_page_info_by_offs(Fs.flash_device, Fs.offset, &info);
-    if (ret) {
-        LOG_ERR("Unable to get page info err %d", ret);
-        while (true)
-            ;
-    }
-    LOG_INF("NVS sector size %u part size %u", info.size, NVS_PARTITION_SIZE);
-
-    Fs.sector_size = info.size;
-    Fs.sector_count = NVS_PARTITION_SIZE / info.size;
-
-    ret = nvs_mount(&Fs);
-    if (0 != ret) {
-        LOG_ERR("Flash Init failed err %d", ret);
-        while (true)
-            ;
-    }
-
-    uint32_t boot_counter = UINT32_C(0);
-    size_t area_len = sizeof(boot_counter);
-    ret = nvs_read(&Fs, NVS_ID_BOOT_COUNT, &boot_counter, area_len);
-    if (ret > 0) { /* item was found, show it */
-        LOG_INF("boot counter: %d", boot_counter);
-    } else { /* item was not found, add it */
-        LOG_INF("No boot counter found, adding it at id %d", NVS_ID_BOOT_COUNT);
-    }
-    boot_counter++;
-
-    if (area_len ==
-        nvs_write(&Fs, NVS_ID_BOOT_COUNT, &boot_counter, area_len)) {
-        LOG_INF("Save boot counter %d succ", boot_counter);
-    } else {
-        LOG_ERR("Save boot counter %d err", boot_counter);
-    }
-
     ret = gpio_pin_configure_dt(&InfoLed, GPIO_OUTPUT_ACTIVE);
     if (0 != ret) {
         LOG_ERR("gpio configuration failed");
